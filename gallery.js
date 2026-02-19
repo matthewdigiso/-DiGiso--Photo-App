cb081a0585f7bed45aa9fd9cb2e55efe8ba7a9e4
Matthew DiGiso <126mdigiso@frhsd.com>
2026-02-19 09:48:07 -0500

    display first image and meta data

--- gallery.js
+++ gallery.js
@@ -22,9 +22,10 @@ $(document).ready(() => {
 
 // Function to fetch JSON data and store it in mImages
 function fetchJSON () {
-  // Use $.getJSON here to request the JSON data from mUrl
-  // On success, parse the JSON and push each image object into mImages array
-  // After JSON is loaded, call swapPhoto() to display the first image
+ $.getJSON(myImages.json, function(data) {
+  mImages = data.images
+  Images=data;
+ })
 }
 
 // Function to swap and display the next photo in the slideshow
